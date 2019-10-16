ef5e30f3e781c1c038723d56b3138aa174f861c6
Khozema Nullwala <khozema.nullwala@accenture.com>
2019-10-16 10:20:24 +0530

    To work with Lab Activity 4.2

--- time_trigger.py
+++ time_trigger.py
@@ -11,7 +11,13 @@ print("Current Time: ", current_time)
 #-----------------------------------------------------
 
 # --------------------------------------------------------------------------------
-# Uncomment the below print statement for trigger exercise 
+# For git trigger exercise follow the below steps - 
+# git checkout develop
+# Modify quote variable with your favourite quote
+# git add --all
+# git commit -m "Your Message"
+# git push
 # --------------------------------------------------------------------------------
-# print("When someone shares something of value with you that benefits you, then you are morally obligated to share it with others!")
+quote = "When someone shares something of value with you that benefits you, then you are morally obligated to share it with others!"
+print(quote)
 # --------------------------------------------------------------------------------
