11eaebcb8276b96dfeb9ececf04ce5643e5c7a56
shilpareddy165 <shilpashree165@gmail.com>
2019-12-22 10:20:29 +0530

    Update time_trigger.py

--- time_trigger.py
+++ time_trigger.py
@@ -18,6 +18,6 @@ print("Current Time: ", current_time)
 # git commit -m "Your Message"
 # git push
 # --------------------------------------------------------------------------------
-quote = "An optimistic man is the one who thinks even bullshit is a fertilizer!"
+quote = "Good Morning!!!!!!!!!!!!!!!!!!"
 print(quote)
 # --------------------------------------------------------------------------------
